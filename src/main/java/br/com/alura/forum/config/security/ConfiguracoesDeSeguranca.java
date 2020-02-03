c8f0d6ffb7759573fb1a8eb427d5e11f3bda3139
vvaugusto <victorhyuuk1@gmail.com>
2020-02-03 18:20:43 -0200

    Implemented Swagger Documentation

--- src/main/java/br/com/alura/forum/config/security/ConfiguracoesDeSeguranca.java
+++ src/main/java/br/com/alura/forum/config/security/ConfiguracoesDeSeguranca.java
@@ -65,6 +65,8 @@ public class ConfiguracoesDeSeguranca extends WebSecurityConfigurerAdapter{
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+        .antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
 
 	}
 	
