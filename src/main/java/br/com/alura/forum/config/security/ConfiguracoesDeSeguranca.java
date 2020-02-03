3014c2e91720be2dabcd89a15b9bd07dd22fe315
vvaugusto <victorhyuuk1@gmail.com>
2020-02-03 17:54:36 -0200

    Implemented Actuator

--- src/main/java/br/com/alura/forum/config/security/ConfiguracoesDeSeguranca.java
+++ src/main/java/br/com/alura/forum/config/security/ConfiguracoesDeSeguranca.java
@@ -43,9 +43,9 @@ public class ConfiguracoesDeSeguranca extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		    .antMatchers(HttpMethod.GET, "/topicos")
+		    .antMatchers(HttpMethod.GET, "/topicos/**")
 		    .permitAll()
-		    .antMatchers(HttpMethod.GET, "/topicos/*")
+		    .antMatchers(HttpMethod.GET, "/actuator/**") 	// não recomendado em produção
 		    .permitAll()
 		    .antMatchers(HttpMethod.POST, "/auth")
 		    .permitAll()
